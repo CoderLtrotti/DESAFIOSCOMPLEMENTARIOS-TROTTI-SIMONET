25362448deb5c989f245fa513f1ec3d3d24aed89
CoderLtrotti <120693492+CoderLtrotti@users.noreply.github.com>
2023-09-28 23:59:07 -0300

    cambios

---
+++ src/dao/models/users.model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: {
+        type:String,
+        unique: true,
+        required: true,
+        index: true,
+    },
+    age: Number,
+    password: String,
+    img: String,
+    cart: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Cart',
+      },
+      role: {
+        type: String,
+        default: 'user',
+      },  
+  });
+
+  const userModel = mongoose.model('users', userSchema);
+
+  export default userModel;
